1548c02ab77684bbbeabc67844e5c43f8be3c828
Azmi <azmianshari123@gmail.com>
2024-12-30 11:25:26 +0700

    Menambahkan MahasiswaApp dan PenyediaViewModel

---
+++ app/src/main/java/com/example/questapi/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.questapi.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(
+                MahasiswaApplication().container.kontakRepository)
+        }
+        initializer {
+            InsertViewModel(
+                MahasiswaApplication().container.kontakRepository)
+        }
+        initializer {
+            DetailViewModel(
+                createSavedStateHandle(),
+                MahasiswaApplication().container.kontakRepository)
+        }
+        initializer {
+            UpdateViewModel(
+                createSavedStateHandle(),
+                MahasiswaApplication().container.kontakRepository)
+        }
+    }
+}
+
+fun CreationExtras.MahasiswaApplication(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
